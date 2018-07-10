25883a1adb7a7900dcbf0ee7d5e0c850c99a5ce4
Alexander Houben <ahouben@greenliff.com>
2018-07-10 02:17:00 +0200

    update README, initial sample code

---
+++ GrpcXamarinAndroidDemo/GrpcXamarinAndroidDemo/MainActivity.cs
@@ -0,0 +1,178 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Speech.V1;
+using Grpc.Core;
+using Grpc.Auth;
+using System.Threading;
+
+namespace GrpcXamarinAndroidDemo
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button buttonMain;
+        private bool isPlaying = false;
+        private CancellationTokenSource cancelPlayingCancellationTokenSource;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            buttonMain = FindViewById<Button>(Resource.Id.buttonHello);
+
+            // add click handler
+            buttonMain.Click += async (_sender, _args) =>
+            {
+                string buttonText = null;
+
+                if (isPlaying)
+                {
+                    // cancel playing
+                    cancelPlayingCancellationTokenSource.Cancel();
+                    return;
+                }
+
+                // guard against exceptions bubbling up
+                try
+                {
+                    // give the user the possibility to cancel playing
+                    cancelPlayingCancellationTokenSource = new CancellationTokenSource();
+                    // remember button text
+                    buttonText = buttonMain.Text;
+                    // set text for cancellation
+                    buttonMain.Text = "Cancel Playing";
+
+                    // start playing
+                    isPlaying = true;
+                    await openChannelToGoogleSpeechApiAndStreamWavFileUntil65SecondsException(cancelPlayingCancellationTokenSource.Token);
+                }
+                catch (Exception)
+                {
+                    // should not happen
+                }
+                finally
+                {
+                    isPlaying = false;
+                    // restore button text
+                    buttonMain.Text = buttonText;
+                }
+            };
+        }
+
+        private async Task openChannelToGoogleSpeechApiAndStreamWavFileUntil65SecondsException(CancellationToken cancellationToken)
+        {
+            try
+            {
+                // required to protect against a System.ArgumentNullException on Xamarin.Android during initialization of type
+                // Microsoft.Extensions.PlatformAbstractions.ApplicationEnvironment in the dependency from Google.Api.Gax (see https://github.com/aspnet/PlatformAbstractions/blob/rel/1.1.0/src/Microsoft.Extensions.PlatformAbstractions/ApplicationEnvironment.cs).
+                // The field initializer for "ApplicationBasePath" calls GetApplicationBasePath() which calls Path.GetFullPath(basePath)
+                // where basePath is AppContext.BaseDirectory which returns null on Xamarin by default (see https://github.com/mono/mono/blob/mono-5.10.0.140/mcs/class/referencesource/mscorlib/system/AppContext/AppContext.cs)
+                AppDomain.CurrentDomain.SetData("APP_CONTEXT_BASE_DIRECTORY", Path.DirectorySeparatorChar.ToString());
+
+                var assembly = typeof(MainActivity).GetTypeInfo().Assembly;
+                var stream = assembly.GetManifestResourceStream("GrpcXamarinAndroidDemo.speech_auth.json");
+                string resultString = null;
+
+                using (var reader = new StreamReader(stream))
+                {
+                    resultString = reader.ReadToEnd();
+                }
+
+                var credential = GoogleCredential.FromJson(resultString);
+
+                if (credential.IsCreateScopedRequired)
+                {
+                    credential = credential.CreateScoped(new[] { "https://www.googleapis.com/auth/cloud-platform" });
+                }
+
+                var channel = new Channel(SpeechClient.DefaultEndpoint.Host,
+                    credential.ToChannelCredentials());
+
+                var speech = SpeechClient.Create(channel, new SpeechSettings());
+
+                var streamingCall = speech.StreamingRecognize();
+                // Write the initial request with the config.
+                await streamingCall.WriteAsync(
+                    new StreamingRecognizeRequest()
+                    {
+                        StreamingConfig = new StreamingRecognitionConfig()
+                        {
+                            Config = new RecognitionConfig()
+                            {
+                                Encoding =
+                                RecognitionConfig.Types.AudioEncoding.Linear16,
+                                SampleRateHertz = 16000,
+                                LanguageCode = "en",
+                            },
+                            InterimResults = true,
+                        }
+                    });
+                // Print responses as they arrive.
+                var printResponses = Task.Run(async () =>
+                {
+                    while (await streamingCall.ResponseStream.MoveNext(
+                        default(CancellationToken)))
+                    {
+                        foreach (var result in streamingCall.ResponseStream
+                            .Current.Results)
+                        {
+                            foreach (var alternative in result.Alternatives)
+                            {
+                                Console.WriteLine(alternative.Transcript);
+                            }
+                        }
+                    }
+                });
+
+                // stream indefinitely
+                while (DateTime.UtcNow > DateTime.MinValue && !cancellationToken.IsCancellationRequested)
+                {
+                    var audioStream = assembly.GetManifestResourceStream("GrpcXamarinAndroidDemo.helloGoogleSpeechAPI.wav");
+
+                    using (var _stream = audioStream)
+                    {
+                        var buffer = new byte[32 * 1024];
+                        int bytesRead;
+                        while ((bytesRead = await _stream.ReadAsync(
+                            buffer, 0, buffer.Length)) > 0 && !cancellationToken.IsCancellationRequested)
+                        {
+                            await streamingCall.WriteAsync(
+                                new StreamingRecognizeRequest()
+                                {
+                                    AudioContent = Google.Protobuf.ByteString
+                                    .CopyFrom(buffer, 0, bytesRead),
+                                });
+                            await Task.Delay(500);
+                        };
+                    }
+                }
+
+                await streamingCall.WriteCompleteAsync();
+                await printResponses;
+            }
+            catch (RpcException rpce)
+            {
+                if (rpce.StatusCode == StatusCode.OutOfRange)
+                {
+                    Console.WriteLine("65 seconds quota limit reached, this exception is expected");
+                }
+                else throw;
+            }
+            catch(System.OperationCanceledException)
+            {
+                // user canceled operation
+            }
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # grpc-xamarin-samples
 Contains demo code showing how grpc works with Xamarin.Android
+
+Steps:
+1. add the following nuget feed for package restore to work: https://www.myget.org/F/greenliff-grpc/api/v3/index.json
+2. get your own API credentials of type "service account" by registering on https://cloud.google.com/video-intelligence/docs/common/auth and saving the credentials to the file GrpcXamarinAndroidDemo/speech_auth.json
+3. Build, run and deploy the application to your mobile, you should see this:
+
+[Screenshot on phone](images/Screenshot.jpg)
+
+with the following output in the debug console after clicking the button in the app:
+
+```
+...
+07-10 01:58:35.919 D/Mono    (18105): DllImport attempting to load: 'grpc_csharp_ext'.
+07-10 01:58:35.922 D/Mono    (18105): DllImport error loading library '/storage/emulated/0/Android/data/GrpcXamarinAndroidDemo.GrpcXamarinAndroidDemo/files/.__override__/libgrpc_csharp_ext': 'dlopen failed: library "/data/app/GrpcXamarinAndroidDemo.GrpcXamarinAndroidDemo-OpMrUrIc9LYA7RSFBbz4Vg==/lib/arm64//storage/emulated/0/Android/data/GrpcXamarinAndroidDemo.GrpcXamarinAndroidDemo/files/.__override__/libgrpc_csharp_ext" not found'.
+07-10 01:58:35.924 D/Mono    (18105): DllImport error loading library '/storage/emulated/0/Android/data/GrpcXamarinAndroidDemo.GrpcXamarinAndroidDemo/files/.__override__/libgrpc_csharp_ext.so': 'dlopen failed: library "/data/app/GrpcXamarinAndroidDemo.GrpcXamarinAndroidDemo-OpMrUrIc9LYA7RSFBbz4Vg==/lib/arm64//storage/emulated/0/Android/data/GrpcXamarinAndroidDemo.GrpcXamarinAndroidDemo/files/.__override__/libgrpc_csharp_ext.so" not found'.
+07-10 01:58:35.926 D/Mono    (18105): DllImport error loading library '/system/lib/libgrpc_csharp_ext': 'dlopen failed: library "/data/app/GrpcXamarinAndroidDemo.GrpcXamarinAndroidDemo-OpMrUrIc9LYA7RSFBbz4Vg==/lib/arm64//system/lib/libgrpc_csharp_ext" not found'.
+07-10 01:58:35.928 D/Mono    (18105): DllImport error loading library '/system/lib/libgrpc_csharp_ext.so': 'dlopen failed: library "/data/app/GrpcXamarinAndroidDemo.GrpcXamarinAndroidDemo-OpMrUrIc9LYA7RSFBbz4Vg==/lib/arm64//system/lib/libgrpc_csharp_ext.so" not found'.
+07-10 01:58:35.931 D/Mono    (18105): DllImport error loading library 'libgrpc_csharp_ext': 'dlopen failed: library "/data/app/GrpcXamarinAndroidDemo.GrpcXamarinAndroidDemo-OpMrUrIc9LYA7RSFBbz4Vg==/lib/arm64/libgrpc_csharp_ext" not found'.
+07-10 01:58:35.937 D/Mono    (18105): DllImport loaded library 'libgrpc_csharp_ext.so'.
+07-10 01:58:35.937 D/Mono    (18105): DllImport searching in: 'grpc_csharp_ext' ('libgrpc_csharp_ext.so').
+07-10 01:58:35.937 D/Mono    (18105): Searching for 'grpcsharp_redirect_log'.
+07-10 01:58:35.937 D/Mono    (18105): Probing 'grpcsharp_redirect_log'.
+07-10 01:58:35.937 D/Mono    (18105): Found as 'grpcsharp_redirect_log'.
+07-10 01:58:35.940 D/Mono    (18105): Assembly Ref addref Grpc.Core[0x7a25933000] -> System.Reflection[0x7a14f32080]: 5
+07-10 01:58:35.952 D/Mono    (18105): DllImport searching in: 'grpc_csharp_ext' ('libgrpc_csharp_ext.so').
+07-10 01:58:35.952 D/Mono    (18105): Searching for 'grpcsharp_override_default_ssl_roots'.
+07-10 01:58:35.952 D/Mono    (18105): Probing 'grpcsharp_override_default_ssl_roots'.
+07-10 01:58:35.952 D/Mono    (18105): Found as 'grpcsharp_override_default_ssl_roots'.
+07-10 01:58:35.963 D/Mono    (18105): DllImport searching in: 'grpc_csharp_ext' ('libgrpc_csharp_ext.so').
+07-10 01:58:35.963 D/Mono    (18105): Searching for 'grpcsharp_init'.
+07-10 01:58:35.963 D/Mono    (18105): Probing 'grpcsharp_init'.
+07-10 01:58:35.963 D/Mono    (18105): Found as 'grpcsharp_init'.
+...
+hello Google speech API ratings from
+07-10 01:59:02.162 I/mono-stdout(18105): hello Google speech API ratings from
+ xamarin Android
+07-10 01:59:02.163 I/mono-stdout(18105):  xamarin Android
+hello Google speech API ratings from xamarin Android
+07-10 01:59:03.155 I/mono-stdout(18105): hello Google speech API ratings from xamarin Android
+07-10 01:59:03.595 I/mono-stdout(18105):  hello Google speech API greetings from xamarin Android hello Google speech API greetings from xamarin Android
+
+ hey07-10 01:59:04.200 I/mono-stdout(18105):  hey
+
+ hell
+07-10 01:59:04.205 I/mono-stdout(18105):  hell
+ hello07-10 01:59:04.214 I/mono-stdout(18105):  hello
+...
+```
